3f1c63ef62bd1ee32ae459ef0b851fc9a25a9353
ridhamviramgami <108387914+ridhamviramgami@users.noreply.github.com>
2022-07-26 11:59:24 +0530

    Add files via upload

---
+++ filtering/filtering.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $(".one").first().css("background-color", "yellow");
+    $("p").last().css("background-color", "yellow");
+});
---
+++ noConflict()/noConflict().js
@@ -0,0 +1,6 @@
+$.noConflict();
+jQuery(document).ready(function(){
+    jQuery("button").click(function() {
+        jQuery("p").text("I am ridham");
+    });
+});
